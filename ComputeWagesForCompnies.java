4983959aa3275d1793e382a115feaaec486cc726
kiran68 <kiranurshil88@gmail.com>
2023-03-29 19:11:46 +0530

    Add: Compute Employee Wage for multiple companies

---
+++ ComputeWagesForCompnies.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.employeewagecomputation;
+
+public class ComputeWagesForCompnies {
+
+	public static  final int IS_PART_TIME = 1;
+	public static final int  IS_FULL_TIME = 2;
+	
+	public static int computeEmpWage (String company,int empRatePerHour,
+			                           int numOfWorkingDays, int maxHourPerMonth) {
+		
+		int empHrs = 0,totalEmpHrs = 0, totalWorkingDays = 0;
+		
+		while (totalEmpHrs <= maxHourPerMonth &&
+				totalWorkingDays < numOfWorkingDays) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case IS_PART_TIME :
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME :
+				empHrs = 8;
+				break;
+				default:
+					empHrs = 0;
+			}
+			totalEmpHrs +=empHrs;
+			System.out.println("Days# :" + "totalWorkingDays" + "Emp Hr:" +empHrs);
+		}
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Total Emp Wage for Comapny :" +company+" is:" +totalEmpWage);
+		return totalEmpWage;
+	}
+	public static void main(String[] args) {
+		computeEmpWage ("AMENORA", 20, 3, 25);
+		computeEmpWage ("PHINIIX", 20,2,23);
+	}	                                      
+			
+}
